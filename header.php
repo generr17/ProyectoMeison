b7e3e17b995d266b72f42ca4eed9d8785e049060
Sebastian Mayorga <sebasmayorga98@gmail.com>
2020-09-18 17:06:03 -0500

    relaciones

--- header.php
+++ header.php
@@ -3,7 +3,7 @@
     <div class="container-fluid">
 
       <div class="logo float-left">
-        <h1 class="text-light"><a href="index.html"><span>Siimple</span></a></h1>
+        <h1 class="text-light"><a href="index.php"><span>Meison</span></a></h1>
         <!-- Uncomment below if you prefer to use an image logo -->
         <!-- <a href="index.html"><img src="assets/img/logo.png" alt="" class="img-fluid"></a>-->
       </div>
@@ -12,15 +12,15 @@
       <nav class="nav-menu">
         <ul>
           <li class="active"><a href="index.html">Home</a></li>
-          <li><a href="nosotros.html">About Us</a></li>
-          <li><a href="historia.html">Why Us</a></li>
+          <li><a href="nosotros.php">About Us</a></li>
+          <li><a href="historia.php">Why Us</a></li>
           
           <li class="drop-down"><a href="">Productos</a>
             <ul>
               <li><a href="#">Drop Down 1</a></li>
               <li class="drop-down"><a href="#">Drop Down 2</a>
                 <ul>
-                  <li><a href="productos.html">Producto 1</a></li>
+                  <li><a href="productos.php">Producto 1</a></li>
                   <li><a href="#">Deep Drop Down 2</a></li>
                   <li><a href="#">Deep Drop Down 3</a></li>
                   <li><a href="#">Deep Drop Down 4</a></li>
@@ -32,8 +32,8 @@
               <li><a href="#">Drop Down 5</a></li>
             </ul>
           </li>
-          <li><a href="preguntas.html">Preguntas Frecuentes</a></li>
-          <li><a href="contacto.html">Contact Us</a></li>
+          <li><a href="preguntas.php">Preguntas Frecuentes</a></li>
+          <li><a href="contacto.php">Contact Us</a></li>
         </ul>
       </nav><!-- .nav-menu -->
 
